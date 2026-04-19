6357595adf26acb882b3f8b81a6f434cb19d8a4d
Lucas <l.k.gralak@proton.me>
2026-04-19 17:45:32 +0200

    New nvim changes + colors with matugen

--- arch/dotfiles/.config/nvim/lua/plugins/buffers.lua
+++ arch/dotfiles/.config/nvim/lua/plugins/buffers.lua
@@ -1,29 +1,29 @@
 -- lua/plugins/buffers.lua
 vim.pack.add({
-  "https://github.com/akinsho/bufferline.nvim",
-  "https://github.com/nvim-tree/nvim-web-devicons",
-  "https://github.com/famiu/bufdelete.nvim",
+	"https://github.com/akinsho/bufferline.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/famiu/bufdelete.nvim",
 })
 
 require("bufferline").setup({
-  options = {
-    diagnostics = "nvim_lsp",
-    always_show_bufferline = false,
-    separator_style = "thin",
-    close_command = function(n)
-      require("bufdelete").bufdelete(n, true)
-    end,
-    right_mouse_command = function(n)
-      require("bufdelete").bufdelete(n, true)
-    end,
-    diagnostics_indicator = function(_, _, diag)
-      local ret = (diag.error and " " .. diag.error .. " " or "") .. (diag.warning and " " .. diag.warning or "")
-      return vim.trim(ret)
-    end,
-    offsets = {
-      { filetype = "neo-tree", text = "Neo-tree", highlight = "Directory", text_align = "left" },
-    },
-  },
+	options = {
+		diagnostics = "nvim_lsp",
+		always_show_bufferline = false,
+		separator_style = "thin",
+		close_command = function(n)
+			require("bufdelete").bufdelete(n, true)
+		end,
+		right_mouse_command = function(n)
+			require("bufdelete").bufdelete(n, true)
+		end,
+		diagnostics_indicator = function(_, _, diag)
+			local ret = (diag.error and " " .. diag.error .. " " or "") .. (diag.warning and " " .. diag.warning or "")
+			return vim.trim(ret)
+		end,
+		offsets = {
+			{ filetype = "neo-tree", text = "Neo-tree", highlight = "Directory", text_align = "left" },
+		},
+	},
 })
 
 local map = vim.keymap.set
@@ -34,9 +34,9 @@ map("n", "<leader>.", "<cmd>BufferLineMoveNext<CR>", { desc = "Move buffer right
 map("n", "<leader>bp", "<cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin" })
 map("n", "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<CR>", { desc = "Delete non-pinned buffers" })
 map("n", "<leader>bd", function()
-  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
-  if #bufs <= 1 then
-    vim.cmd("enew") -- open empty buffer first
-  end
-  require("bufdelete").bufdelete(0, true)
+	local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+	if #bufs <= 1 then
+		vim.cmd("enew") -- open empty buffer first
+	end
+	require("bufdelete").bufdelete(0, true)
 end, { desc = "Delete buffer" })
