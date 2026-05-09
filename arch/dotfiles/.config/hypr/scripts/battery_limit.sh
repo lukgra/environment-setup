ab11b9ad7cc4aa4d87282ae811158a489ad414d2
Lucas <l.k.gralak@proton.me>
2026-05-09 17:41:42 +0200

    Move to DMS

--- arch/dotfiles/.config/hypr/scripts/battery_limit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENT=$(cat /sys/class/power_supply/BAT0/charge_control_end_threshold)
-
-if [ "$CURRENT" -le 80 ]; then
-    echo 100 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
-else
-    echo 80 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
-fi
