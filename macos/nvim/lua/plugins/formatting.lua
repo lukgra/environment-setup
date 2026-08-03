8f5c07741479ca12850a80e4e67ceb66547fb986
Lucas <lukgralak@gmail.com>
2026-08-03 19:25:23 +0200

    autformatting off

--- macos/nvim/lua/plugins/formatting.lua
+++ macos/nvim/lua/plugins/formatting.lua
@@ -3,7 +3,6 @@ vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
 
 require("conform").setup({
   notify_on_error = false,
-  format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "ruff_format" },
